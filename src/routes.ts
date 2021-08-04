c2efb13f0e61907045d57d6abe04d7d5f8f58884
Fisrael <felipe.israel@wearepsh.com>
2021-08-04 16:59:04 -0300

    Add ping controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import Router = require("koa-router");
 import productController from "./controllers/product";
+import pingController from "./controllers/ping";
 
 const router = new Router({
 	prefix: '/api'
 });
 
 router.use(productController)
+router.use(pingController)
 
 export default router;
